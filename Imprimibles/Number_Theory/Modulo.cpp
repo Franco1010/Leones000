dcfc5126ee065710322bca220054053c3be59314
crystaalroo <37081894+crystaalroo@users.noreply.github.com>
2020-10-20 00:46:46 -0500

    Create Modulo.cpp

---
+++ Imprimibles/Number_Theory/Modulo.cpp
@@ -0,0 +1,90 @@
+using uint=unsigned;
+using ull=unsigned long long;
+struct modinfo{uint mod,root;};
+template<modinfo const&ref>
+struct modular{
+	static constexpr uint const &mod=ref.mod;
+	static modular root(){return modular(ref.root);}
+	uint v;
+	//modular(initializer_list<uint>ls):v(*ls.bg){}
+	modular(lli vv=0){s(vv%mod+mod);}
+	modular& s(uint vv){
+		v=vv<mod?vv:vv-mod;
+		return *this;
+	}
+	modular operator-()const{return modular()-*this;}
+	modular& operator+=(const modular&rhs){return s(v+rhs.v);}
+	modular&operator-=(const modular&rhs){return s(v+mod-rhs.v);}
+	modular&operator*=(const modular&rhs){
+		v=ull(v)*rhs.v%mod;
+		return *this;
+	}
+	modular&operator/=(const modular&rhs){return *this*=rhs.inv();}
+	modular operator+(const modular&rhs)const{return modular(*this)+=rhs;}
+	modular operator-(const modular&rhs)const{return modular(*this)-=rhs;}
+	modular operator*(const modular&rhs)const{return modular(*this)*=rhs;}
+	modular operator/(const modular&rhs)const{return modular(*this)/=rhs;}
+	modular pow(int n)const{
+		modular res(1),x(*this);
+		while(n){
+			if(n&1)res*=x;
+			x*=x;
+			n>>=1;
+		}
+		return res;
+	}
+	modular inv()const{return pow(mod-2);}
+
+	friend modular operator+(int x,const modular&y){
+		return modular(x)+y;
+	}
+	friend modular operator-(int x,const modular&y){
+		return modular(x)-y;
+	}
+	friend modular operator*(int x,const modular&y){
+		return modular(x)*y;
+	}
+	friend modular operator/(int x,const modular&y){
+		return modular(x)/y;
+	}
+	friend ostream& operator<<(ostream&os,const modular&m){
+		return os<<m.v;
+	}
+	friend istream& operator>>(istream&is,modular&m){
+		lli x;is>>x;
+		m=modular(x);
+		return is;
+	}
+	bool operator<(const modular&r)const{return v<r.v;}
+	bool operator==(const modular&r)const{return v==r.v;}
+	bool operator!=(const modular&r)const{return v!=r.v;}
+	explicit operator bool()const{
+		return v;
+	}
+};
+
+//extern constexpr modinfo base{998244353,3};
+extern constexpr modinfo base{1000000007,0};
+//modinfo base{1,0};
+using mint=modular<base>;
+
+//COMBI
+
+const int vmax=(1<<21)+10;
+mint fact[vmax],finv[vmax],invs[vmax];
+void initfact(){
+	fact[0]=1;
+	fore(i,1,vmax)fact[i]=fact[i-1]*i;
+	finv[vmax-1]=fact[vmax-1].inv();
+	for(int i=vmax-2;i>=0;i--)finv[i]=finv[i+1]*(i+1);
+	for(int i=vmax-1;i>=1;i--)invs[i]=finv[i]*fact[i-1];
+}
+mint choose(int n,int k){
+	return fact[n]*finv[n-k]*finv[k];
+}
+mint binom(int a,int b){
+	return fact[a+b]*finv[a]*finv[b];
+}
+mint catalan(int n){
+	return binom(n,n)-(n-1>=0?binom(n-1,n+1):0);
+}
