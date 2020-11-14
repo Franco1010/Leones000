e435d5d8cb56a723544ed9da257fbf7c55a7d372
crystaalroo <37081894+crystaalroo@users.noreply.github.com>
2020-11-14 16:25:32 -0600

    Update pollard.cpp

--- Imprimibles/Number_Theory/pollard.cpp
+++ Imprimibles/Number_Theory/pollard.cpp
@@ -1,8 +1,8 @@
 struct Factorize {
     using lll = __int128_t;
     random_device rnd;
-    ll modpow(ll a, ll n, ll mod) {
-        ll ret = 1;
+    lli modpow(lli a, lli n, lli mod) {
+        lli ret = 1;
         while (n > 0) {
             if (n & 1) ret = lll(ret) * lll(a) % lll(mod);
             a = lll(a) * lll(a) % lll(mod);
@@ -10,13 +10,13 @@ struct Factorize {
         }
         return ret;
     }
-    ll Rho(ll n) {
-        auto f = [&](ll x) { return (lll(x) * lll(x) + lll(1)) % lll(n); };
+    lli Rho(lli n) {
+        auto f = [&](lli x) { return (lll(x) * lll(x) + lll(1)) % lll(n); };
         while (1) {
-            ll x = uniform_int_distribution<ll>(0, n - 1)(rnd);
-            ll y = f(x);
+            lli x = uniform_int_distribution<lli>(0, n - 1)(rnd);
+            lli y = f(x);
             while (1) {
-                ll d = gcd(abs(x - y), n);
+                lli d = gcd(abs(x - y), n);
                 if (d == n) break;
                 if (1 < d) return d;
                 x = f(x);
@@ -24,43 +24,43 @@ struct Factorize {
             }
         }
     }
-    inline bool Miller_Rabin(ll n) {
+    inline bool Miller_Rabin(lli n) {
         if (n == 1) return 0;
-        ll d = n - 1, s = 0;
+        lli d = n - 1, s = 0;
         while (~d & 1) d >>= 1, s++;
-        auto check = [&](ll a) {
-            ll x = modpow(a, d, n);
+        auto check = [&](lli a) {
+            lli x = modpow(a, d, n);
             if (x == 1) return 1;
-            ll y = n - 1;
+            lli y = n - 1;
             for (int i = 0; i < s; i++) {
                 if (x == y) return 1;
                 x = lll(x) * lll(x) % lll(n);
             }
             return 0;
         };
-        for (ll a : {2, 325, 9375, 28178, 450775, 9780504, 1795265022}) {
+        for (lli a : {2, 325, 9375, 28178, 450775, 9780504, 1795265022}) {
             if (a >= n) break;
             if (!check(a)) return 0;
         }
         return 1;
     }
-    map<ll, int> operator()(ll n) {
-        map<ll, int> ret;
+    map<lli, int> operator()(lli n) {
+        map<lli, int> ret;
         while (~n & 1) {
             n >>= 1;
             ret[2]++;
         }
-        queue<ll> q;
+        queue<lli> q;
         q.push(n);
         while (sz(q)) {
-            ll now = q.front();
+            lli now = q.front();
             q.pop();
             if (now == 1) continue;
             if (Miller_Rabin(now)) {
                 ret[now]++;
                 continue;
             }
-            ll p = Rho(now);
+            lli p = Rho(now);
             q.push(p);
             q.push(now / p);
         }
