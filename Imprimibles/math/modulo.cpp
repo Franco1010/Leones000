a2b885eadd94462632a229cba4936df860b98063
crystaalroo <37081894+crystaalroo@users.noreply.github.com>
2020-12-21 16:34:46 -0600

    Create modulo.cpp

---
+++ Imprimibles/math/modulo.cpp
@@ -0,0 +1,69 @@
+using uint=unsigned;
+using ull=unsigned long long;
+struct modinfo{uint mod,root;};
+template<modinfo const&ref>
+struct modular{
+	static constexpr uint const &mod=ref.mod;
+	static modular root(){return modular(ref.root);}
+	uint v;
+	//modular(initializer_list<uint>ls):v(*ls.bg){}
+	modular(lli vv=0){s(vv%mod+mod);}
+	modular& s(uint vv){
+		v=vv<mod?vv:vv-mod;
+		return *this;
+	}
+	modular operator-()const{return modular()-*this;}
+	modular& operator+=(const modular&rhs){return s(v+rhs.v);}
+	modular&operator-=(const modular&rhs){return s(v+mod-rhs.v);}
+	modular&operator*=(const modular&rhs){
+		v=ull(v)*rhs.v%mod;
+		return *this;
+	}
+	modular&operator/=(const modular&rhs){return *this*=rhs.inv();}
+	modular operator+(const modular&rhs)const{return modular(*this)+=rhs;}
+	modular operator-(const modular&rhs)const{return modular(*this)-=rhs;}
+	modular operator*(const modular&rhs)const{return modular(*this)*=rhs;}
+	modular operator/(const modular&rhs)const{return modular(*this)/=rhs;}
+	modular pow(int n)const{
+		modular res(1),x(*this);
+		while(n){
+			if(n&1)res*=x;
+			x*=x;
+			n>>=1;
+		}
+		return res;
+	}
+	modular inv()const{return pow(mod-2);}
+
+	friend modular operator+(int x,const modular&y){
+		return modular(x)+y;
+	}
+	friend modular operator-(int x,const modular&y){
+		return modular(x)-y;
+	}
+	friend modular operator*(int x,const modular&y){
+		return modular(x)*y;
+	}
+	friend modular operator/(int x,const modular&y){
+		return modular(x)/y;
+	}
+	friend ostream& operator<<(ostream&os,const modular&m){
+		return os<<m.v;
+	}
+	friend istream& operator>>(istream&is,modular&m){
+		lli x;is>>x;
+		m=modular(x);
+		return is;
+	}
+	bool operator<(const modular&r)const{return v<r.v;}
+	bool operator==(const modular&r)const{return v==r.v;}
+	bool operator!=(const modular&r)const{return v!=r.v;}
+	explicit operator bool()const{
+		return v;
+	}
+};
+
+//extern constexpr modinfo base{998244353,3};
+extern constexpr modinfo base{1000000007,0};
+//modinfo base{1,0};
+using mint=modular<base>;
