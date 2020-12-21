3c8d86933303805ec87793999f42bbab94195e37
crystaalroo <37081894+crystaalroo@users.noreply.github.com>
2020-12-21 16:29:56 -0600

    Update minmax_queue.cpp
    version cpp

--- Imprimibles/Data_Structures/minmax_queue.cpp
+++ Imprimibles/Data_Structures/minmax_queue.cpp
@@ -1,16 +1,26 @@
-struct min_queue: deque<ii> { // TODO: rehacer con version CP-algo // O(N)
-  void add(ii cur){ // add a element to the right {v, pos}
-    while( !empty() && back().f >= cur.f ){
-      pop_back();
-    }
-    push_back(cur);
-  }
-  void rem(int i){// remove all less than i, O(K)
-    while( front().s < s ){
-      pop_front();
-    }
-  }
-  lli qmin(){ // min[l, r], O(K)
-    return front().f;
-  }
-} mnqu;
+struct qiu{
+	stack<ii> s1,s2;
+	int query(){
+		int minimum;
+		if (s1.empty() || s2.empty()) minimum = s1.empty() ? s2.top().second : s1.top().second;
+		else minimum = min(s1.top().second, s2.top().second);
+		return minimum;
+	}
+
+	void pop(){
+		if (s2.empty())while (!s1.empty()) {
+        int element = s1.top().first;
+        s1.pop();
+        int minimum = s2.empty() ? element : min(element, s2.top().second);
+        s2.push({element, minimum});
+    	}
+		int remove_element = s2.top().first;
+		s2.pop();
+	}
+
+	void add(int new_element){
+		int minimum = s1.empty() ? new_element : min(new_element, s1.top().second);
+		s1.push({new_element, minimum});
+	}
+};
+
