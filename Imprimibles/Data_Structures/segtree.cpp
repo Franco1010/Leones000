bb7585cdd62e675aa487c7e6f4f5f30b02f7d201
crystaalroo <37081894+crystaalroo@users.noreply.github.com>
2020-12-21 16:28:04 -0600

    Update segtree.cpp
    Typedef

--- Imprimibles/Data_Structures/segtree.cpp
+++ Imprimibles/Data_Structures/segtree.cpp
@@ -1,29 +1,26 @@
-struct Segtree{ // TODO: poder Monoid y F = function<Monoid(Monoid, Monoid)>;
-  #define mid (l + r) / 2
-  #define left(u) (u + 1)
-  #define right(u) (u + ((mid - l + 1) << 1))
-  struct Node{
-    lli s, mx;
-    Node(lli s = 0, lli mx = -1): s(s), mx(mx) {}
-    Node operator + (const Node &n){
-      return Node(s + n.s, max(mx, n.mx));
-    }
-  };
-  vector<Node> st;
-  Segtree(int n): st(2 * n) {}
-  void update(int u, int l, int r, int kth, lli val){ // O(logN)
-    if(l == r){
-      st[u].s = val;
-      st[u].mx = val;
-      return;
-    }
-    if(kth <= mid) update(left(u), l, mid, kth, val);
-    else update(right(u), mid + 1, r, kth, val);
-    st[u] = st[left(u)] + st[right(u)];
-  }
-  Node query(int u, int l, int r, int ll, int rr){ // O(logN)
-    if(l > r or r < ll or l > rr) return Node();
-    if(ll <= l and r <= rr) return st[u];
-    return query(left(u), l, mid, ll, rr) + query(right(u), mid + 1, r, ll, rr);
-  }
+
+template <typename T> struct ST{
+	#define mid (l + r) / 2
+	#define left(u) (u + 1)
+	#define right(u) (u + ((mid - l + 1) << 1))
+	vector<T> st;
+	T bot;
+	ST(int n, T ini, T bot):st(2*n, ini),bot(bot){}
+
+	T f(T a, T b){ return max(a,b);}
+
+	void update(int u, int l, int r, int v, T value){
+		if(l>r)return;
+		if(l==r){st[u]=value; return;}
+		if(v<=mid)update(left(u), l , mid , v , value);
+		else update(right(u), mid+1 , r , v , value);
+		st[u]=f(st[left(u)], st[right(u)]);
+	}
+
+	T query(int u, int l, int r, int ll, int rr){
+		if(l>rr or r<ll or l>r)return bot;
+		if(ll<=l and r<=rr)return st[u];
+		return f(query(left(u), l , mid , ll , rr) , query(right(u), mid+1 , r , ll , rr) );
+	}
+
 };
