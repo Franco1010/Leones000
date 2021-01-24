099192cab43374ed6362ec491d029805d743a743
Franco1010 <franco.ramirez@alumno.udg.mx>
2021-01-23 19:36:29 -0600

    corrections due to MLE in query

--- Imprimibles/Data_Structures/sparse_segtree.cpp
+++ Imprimibles/Data_Structures/sparse_segtree.cpp
@@ -1,36 +1,36 @@
 #define mid l + (r - l) / 2
-typedef struct Node *pNode;
-struct Node {
-	lli s, mx;
-	pNode l, r;
-  Node(lli s = 0, lli mx = -INF): s(s), mx(mx), l(0), r(0) {}
+typedef struct ST *PST;
+struct Node{
+  lli s, mx;
+  Node(lli s = 0, lli mx = 0): s(s), mx(mx) {};
+  Node operator + (const Node& n) {
+    return Node(s + n.s, max(mx, n.mx));
+  }
 };
-lli s(pNode u){return u ? u->s : 0;}
-lli mx(pNode u){return u ? u->mx : -INF;}
-void pull(pNode& u, pNode l, pNode r){
-  u->s = s(l) + s(r);
-  u->mx = max(mx(l), mx(r));
-}
-void update(pNode& u, lli l, lli r, lli kth, lli val){
+struct ST {
+	Node data;
+	PST l, r;
+  ST(Node data = Node()): data(data), l(0), r(0) {}
+};
+Node data(PST& u){return u ? u->data : Node();}
+void pull(PST& u){u->data = data(u->l) + data(u->r);}
+void update(PST& u, lli l, lli r, lli kth, Node val){
   if(l == r){
-    u->s = val;
-    u->mx = val;
+    u->data = val;
     return;
   }
   if(kth <= mid){
-    if(!u->l) u->l = new Node();
+    if(!u->l) u->l = new ST();
     update(u->l, l, mid, kth, val);
   }
   else {
-    if(!u->r) u->r = new Node();
+    if(!u->r) u->r = new ST();
     update(u->r, mid + 1, r, kth, val);
   }
-  pull(u, u->l, u->r);
+  pull(u);
 }
-pNode query(pNode u, lli l, lli r, lli ll, lli rr){
-  if(!u or l > r or r < ll or l > rr) return new Node();
-  if(ll <= l and r <= rr) return u;
-  pNode res = new Node();
-  pull(res, query(u->l, l, mid, ll, rr), query(u->r, mid + 1, r, ll, rr));
-  return res;
+Node query(PST& u, lli l, lli r, lli ll, lli rr){
+  if(!u or l > r or r < ll or l > rr) return Node();
+  if(ll <= l and r <= rr) return u->data;
+  return query(u->l, l, mid, ll, rr) + query(u->r, mid + 1, r, ll, rr);
 }
