ba4d001dd228896c1b1c8c19bd95148954a7ac2c
Franco1010 <franco.ramirez@alumno.udg.mx>
2021-05-08 10:20:51 -0500

    adding point and line

---
+++ Imprimibles/Geometry/point.cpp
@@ -0,0 +1,28 @@
+struct pt {  // for 3D add z coordinate
+	double x,y;
+	pt(double x, double y):x(x),y(y){}
+	pt(){}
+	double norm2(){return *this**this;}
+	double norm(){return sqrt(norm2());}
+	bool operator==(pt p){return abs(x-p.x)<=EPS&&abs(y-p.y)<=EPS;}
+	pt operator+(pt p){return pt(x+p.x,y+p.y);}
+	pt operator-(pt p){return pt(x-p.x,y-p.y);}
+	pt operator*(double t){return pt(x*t,y*t);}
+	pt operator/(double t){return pt(x/t,y/t);}
+	double operator*(pt p){return x*p.x+y*p.y;}
+//	pt operator^(pt p){ // only for 3D
+//		return pt(y*p.z-z*p.y,z*p.x-x*p.z,x*p.y-y*p.x);}
+	double angle(pt p){ // redefine acos for values out of range
+		return acos(*this*p/(norm()*p.norm()));}
+	pt unit(){return *this/norm();}
+	double operator%(pt p){return x*p.y-y*p.x;}
+	// 2D from now on
+	bool operator<(pt p)const{ // for convex hull
+		return x<p.x-EPS||(abs(x-p.x)<=EPS&&y<p.y-EPS);}
+	bool left(pt p, pt q){ // is it to the left of directed line pq?
+		return (q-p)%(*this-p)>EPS;}
+	pt rot(pt r){return pt(*this%r,*this*r);}
+	pt rot(double a){return rot(pt(sin(a),cos(a)));}
+};
+pt ccw90(1,0);
+pt cw90(-1,0);
