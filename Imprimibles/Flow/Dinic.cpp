75a866e881fcc23b1c36bfbf016f9f2bb5197b58
Franco1010 <franco.ramirez@alumno.udg.mx>
2021-06-23 00:46:36 -0500

    dinic

--- Imprimibles/Flow/Dinic.cpp
+++ Imprimibles/Flow/Dinic.cpp
@@ -1,75 +1,47 @@
-template <typename flow_t> struct Dinic {
-    const flow_t INF;
-
-    struct edge {
-        int to;
-        flow_t cap;
-        int rev;
-        bool isrev;
-        int idx;
-    };
-
-    vector<vector<edge>> graph;
-    vector<int> min_cost, iter;
-
-    Dinic(int V) : INF(numeric_limits<flow_t>::max()), graph(V) {}
-
-    void add(int from, int to, flow_t cap, int idx = -1) {
-        graph[from].emplace_back((edge){to, cap, (int)graph[to].size(), false, idx});
-        graph[to].emplace_back((edge){from, 0, (int)graph[from].size() - 1, true, idx});
-    }
-
-    bool bfs(int s, int t) {
-        min_cost.assign(graph.size(), -1);
-        queue<int> que;
-        min_cost[s] = 0;
-        que.push(s);
-        while(!que.empty() && min_cost[t] == -1) {
-            int p = que.front();
-            que.pop();
-            for(auto &e : graph[p]) {
-                if(e.cap > 0 && min_cost[e.to] == -1) {
-                    min_cost[e.to] = min_cost[p] + 1;
-                    que.push(e.to);
-                }
-            }
-        }
-        return min_cost[t] != -1;
-    }
-
-    flow_t dfs(int idx, const int t, flow_t flow) {
-        if(idx == t) return flow;
-        for(int &i = iter[idx]; i < graph[idx].size(); i++) {
-            edge &e = graph[idx][i];
-            if(e.cap > 0 && min_cost[idx] < min_cost[e.to]) {
-                flow_t d = dfs(e.to, t, min(flow, e.cap));
-                if(d > 0) {
-                    e.cap -= d;
-                    graph[e.to][e.rev].cap += d;
-                    return d;
-                }
-            }
-        }
-        return 0;
-    }
-
-    flow_t max_flow(int s, int t) {
-        flow_t flow = 0;
-        while(bfs(s, t)) {
-            iter.assign(graph.size(), 0);
-            flow_t f = 0;
-            while((f = dfs(s, t, INF)) > 0) flow += f;
-        }
-        return flow;
-    }
-
-    void output() {
-        for(int i = 0; i < graph.size(); i++) {
-            for(auto &e : graph[i]) {
-                if(e.isrev) continue;
-                auto &rev_e = graph[e.to][e.rev];
-                cout << i << "->" << e.to << " (flow: " << rev_e.cap << "/" << e.cap + rev_e.cap << ")" << ENDL;
-            }
-        }
-    }
+// Min cut: nodes with dist>=0 vs nodes with dist<0
+// Matching MVC: left nodes with dist<0 + right nodes with dist>0
+struct Dinic{
+	int nodes,src,dst;
+	vector<int> dist,q,work;
+	struct edge {int to,rev;lli f,cap;};
+	vector<vector<edge>> g;
+	Dinic(int x):nodes(x),g(x),dist(x),q(x),work(x){}
+	void add_edge(int s, int t, lli cap){
+		g[s].pb((edge){t,sz(g[t]),0,cap});
+		g[t].pb((edge){s,sz(g[s])-1,0,0});
+	}
+	bool dinic_bfs(){
+		fill(all(dist),-1);dist[src]=0;
+		int qt=0;q[qt++]=src;
+		for(int qh=0;qh<qt;qh++){
+			int u=q[qh];
+			fore(i,0,sz(g[u])){
+				edge &e=g[u][i];int v=g[u][i].to;
+				if(dist[v]<0&&e.f<e.cap)dist[v]=dist[u]+1,q[qt++]=v;
+			}
+		}
+		return dist[dst]>=0;
+	}
+	lli dinic_dfs(int u, lli f){
+		if(u==dst)return f;
+		for(int &i=work[u];i<sz(g[u]);i++){
+			edge &e=g[u][i];
+			if(e.cap<=e.f)continue;
+			int v=e.to;
+			if(dist[v]==dist[u]+1){
+				lli df=dinic_dfs(v,min(f,e.cap-e.f));
+				if(df>0){e.f+=df;g[v][e.rev].f-=df;return df;}
+			}
+		}
+		return 0;
+	}
+	lli max_flow(int _src, int _dst){
+		src=_src;dst=_dst;
+		lli result=0;
+		while(dinic_bfs()){
+			fill(all(work),0);
+			while(lli delta=dinic_dfs(src,INF))result+=delta;
+		}
+		return result;
+	}
 };
