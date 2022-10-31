cdaa34ba09c88fcaa15fd40b418d22373efca3f7
Jorge Iván Bórquez Meza <ivanborquezm@gmail.com>
2022-10-30 22:57:09 -0600

    Change of dijkstra.cpp
    Change the code to the one we normally use

--- Imprimibles/Graphs/dijkstra.cpp
+++ Imprimibles/Graphs/dijkstra.cpp
@@ -1,19 +1,19 @@
-vector<pair<int,int> > g[MAXN];  // u->[(v,cost)]
-long long dist[MAXN];
-void dijkstra(int x){
-	memset(dist,-1,sizeof(dist));
-	priority_queue<pair<long long,int> > q;
-	dist[x]=0;
-    q.push({0,x});
-	while(!q.empty()){
-		x=q.top().s;
-        long long c=-q.top().f;
-        q.pop();
-		if(dist[x]!=c)continue;
-		fore(i,0,g[x].size()){
-			int y=g[x][i].f; ll c=g[x][i].s;
-			if(dist[y]<0||dist[x]+c<dist[y])
-				dist[y]=dist[x]+c,q.push({-dist[y],y});
-		}
+lli n;
+vector<vector<ii>> g;
+vi dijkstra(int u) {
+	vi dist(n, -1);
+	dist[u] = 0;
+	priority_queue<ii, vector<ii>, greater<ii>> pq;
+	pq.push({0, u});
+	while(!pq.empty()) {
+		ii cur = pq.top(); 
+		pq.pop();
+    for(auto it:g[cur.s]) {
+      if(dist[it.f] == -1 or dist[it.f] > (cur.f + it.s)) {
+        dist[it.f] = cur.f + it.s;
+        pq.push({dist[it.f], it.f});
+      }
+    }
 	}
+  return dist;
 }
